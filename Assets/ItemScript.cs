e2dafa4379b9dd67549e6905f09ca0cd9ca66fdf
onknjpeel <k023g0103@g.neec.ac.jp>
2024-06-10 13:13:09 +0900

    6/10授業分

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("Get");
+        //DestroySelf();
+        //Debug.Log("Enter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -6,6 +6,7 @@ public class PullingJump : MonoBehaviour
 {
     private Vector3 clickPosition;
     private Rigidbody rb;
+    private bool isCanJump;
     [SerializeField]
     private float jumpPower = 10;
     // Start is called before the first frame update
@@ -21,11 +22,41 @@ public class PullingJump : MonoBehaviour
         {
             clickPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (isCanJump && Input.GetMouseButtonUp(0))
         {
             Vector3 dist = clickPosition - Input.mousePosition;
             if (dist.sqrMagnitude == 0) { return; }
             rb.velocity = dist.normalized * jumpPower;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("è’ìÀÇµÇΩ");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("ê⁄êGíÜ");
+
+        ContactPoint[] contact = collision.contacts;
+
+        Vector3 otherNormmal = contact[0].normal;
+
+        Vector3 upVector = new Vector3(0, 1, 0);
+
+        float dotUN = Vector3.Dot(upVector, otherNormmal); ;
+
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+
+        if (dotDeg <= 45)
+        {
+            isCanJump = true;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log("ó£íEÇµÇΩ");
+        isCanJump = false;
     }
 }
